0e577bca98ecb6f69852d9d0cdac38f1c563a08b
Solenoid <maruisansmai@gmail.com>
2020-07-10 13:14:01 +0900

    基本ファイルの調整
    rename: dotfilesLink.zsh -> init.zsh

---
+++ init.zsh
@@ -0,0 +1,6 @@
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/shell/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.jupyter ~/.jupyter
+ln -sf ~/dotfiles/.vscode-server ~/.vscode-server
