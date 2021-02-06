4f40f0c1c55d3a1e154858a9d71bde6de3a6e9ba
mikan.ichinose <maruisansmai@gmail.com>
2021-02-06 13:03:40 +0900

    [update] asdf: インストールスクリプトにプラグイン設定を追加

--- asdf/install_asdf.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install curl git
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
--- install_asdf.sh
+++ install_asdf.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sudo apt install curl git
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
 
 node(){
   bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
@@ -6,7 +8,16 @@ node(){
 php(){
   sudo apt update
   sudo apt install -y autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev
+  if command -v brew &> /dev/null; then
+    brew install autoconf automake bison freetype gd gettext icu4c krb5 libedit libiconv libjpeg libpng libxml2 libzip pkg-config re2c zlib
+  fi
 }
 
 node
 php
+
+# install plugins
+cat ~/.tool-versions | awk '{print $1}' | while read plugin
+do
+  asdf add $plugin
+done
