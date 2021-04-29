1c55663a533f6167ada44f689c59a1170430d143
mikan.ichinose <maruisansmai@gmail.com>
2021-04-29 16:58:49 +0900

    update(script): OS依存する部分を分離

--- create_links.sh
+++ create_links.sh
@@ -16,9 +16,14 @@ make_shim ~/.dotfiles/.gitconfig ~/.gitconfig
 make_shim ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 make_shim ~/.dotfiles/nvim ~/.config/nvim
 make_shim ~/.dotfiles/starship/starship.toml ~/.config/starship.toml
-make_shim ~/.dotfiles/asdf/.tool-versions ~/.tool-versions
-make_shim ~/.dotfiles/shell/.profile ~/.profile
-make_shim ~/.dotfiles/shell/.bashrc ~/.bashrc
 make_shim ~/.dotfiles/shell/fish ~/.config/fish
-make_shim ~/.dotfiles/i3 ~/.config/i3
-make_shim ~/.dotfiles/i3blocks ~/.config/i3blocks
+
+if [ `uname` == 'Darwin' ]; then
+  exit
+elif [ `uname` == 'Linux' ]; then
+  make_shim ~/.dotfiles/asdf/.tool-versions ~/.tool-versions
+  make_shim ~/.dotfiles/i3 ~/.config/i3
+  make_shim ~/.dotfiles/i3blocks ~/.config/i3blocks
+  make_shim ~/.dotfiles/shell/.bashrc ~/.bashrc
+  make_shim ~/.dotfiles/shell/.profile ~/.profile
+fi
