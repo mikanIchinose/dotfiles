4de1155064a700342df666e77b3670fde6947985
mikan.ichinose <maruisansmai@gmail.com>
2021-04-29 17:31:07 +0900

    update(asdf): tool-versionsをLinux用とMaｃ用に分離

--- create_links.sh
+++ create_links.sh
@@ -16,13 +16,13 @@ make_shim ~/.dotfiles/.gitconfig ~/.gitconfig
 make_shim ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 make_shim ~/.dotfiles/nvim ~/.config/nvim
 make_shim ~/.dotfiles/starship.toml ~/.config/starship.toml
-make_shim ~/.dotfilesmake_shim ~/.dotfiles/shell/.profile ~/.profile
-/shell/fish ~/.config/fish
+make_shim ~/.dotfiles/shell/fish ~/.config/fish
 
 if [ `uname` == 'Darwin' ]; then
+  make_shim ~/.dotfiles/asdf/.tool-versions.darwin ~/.tool-versions
   exit
 elif [ `uname` == 'Linux' ]; then
-  make_shim ~/.dotfiles/asdf/.tool-versions ~/.tool-versions
+  make_shim ~/.dotfiles/asdf/.tool-versions.linux ~/.tool-versions
   make_shim ~/.dotfiles/i3 ~/.config/i3
   make_shim ~/.dotfiles/i3blocks ~/.config/i3blocks
   make_shim ~/.dotfiles/shell/.bashrc ~/.bashrc
