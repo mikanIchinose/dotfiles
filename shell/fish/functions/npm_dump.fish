715c476f9594b7d0c32a0ad07dc1a0bfb620b30b
mikan.ichinose <maruisansmai@gmail.com>
2021-04-29 17:55:43 +0900

    update(npm): スクリプトをLinux用とMac用に分離

--- shell/fish/functions/npm_dump.fish
+++ shell/fish/functions/npm_dump.fish
@@ -1,3 +1,9 @@
 function npm_dump
-  npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' > $HOME/.dotfiles/npmfile.darwin
+  switch (uname)
+  case Linux
+    set npmfile "$HOME/.dotfiles/npmfile.linux"
+  case Darwin
+    set npmfile "$HOME/.dotfiles/npmfile.darwin"
+  end
+  npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' > $npmfile
 end
--- shell/fish/functions/npm_restore.fish
+++ shell/fish/functions/npm_restore.fish
@@ -1,3 +1,9 @@
 function npm_restore
-  xargs npm install --global < $HOME/.dotfiles/npmfile.darwin
+  switch (uname)
+  case Linux
+    set npmfile "$HOME/.dotfiles/npmfile.linux"
+  case Darwin
+    set npmfile "$HOME/.dotfiles/npmfile.darwin"
+  end
+  xargs npm install --global < $npmfile
 end
