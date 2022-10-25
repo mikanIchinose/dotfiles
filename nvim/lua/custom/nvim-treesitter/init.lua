2a7151fccdc4bd2bf4138d07ea9a57392ab5fd65
Ichinose Yoshihiro <maruisansmai@gmail.com>
2022-10-26 07:00:05 +0900

    update

--- nvim/lua/custom/nvim-treesitter/init.lua
+++ nvim/lua/custom/nvim-treesitter/init.lua
@@ -20,12 +20,13 @@ local config = {
 }
 
 local ts_comment_string_ok, _ = pcall(require, 'ts_comment_string')
-local ts_rainbow_ok, _ = pcall(require, 'rainbow')
 if ts_comment_string_ok then
   config.context_commentstring = {
     enable = true,
   }
 end
+
+local ts_rainbow_ok, _ = pcall(require, 'rainbow')
 if ts_rainbow_ok then
   config.rainbow = {
     enable = true,
@@ -33,37 +34,5 @@ if ts_rainbow_ok then
     max_file_lines = 10000,
   }
 end
+
 require('nvim-treesitter.configs').setup(config)
--- require('nvim-treesitter.configs').setup({
---   ensure_installed = 'all',
---   ignore_install = { 'rnoweb', 'phpdoc' },
---   highlight = {
---     enable = true,
---     disable = {
---       'lua',
---       -- 'vim',
---       'toml',
---       -- 'fish',
---     },
---     additional_vim_regex_highlighting = false,
---   },
---   incremental_selection = {
---     enable = true,
---   },
---   indent = {
---     enable = false,
---   },
---   -- tree_setter = {
---   --   enable = true,
---   -- },
---   -- JoosepAlviste/nvim-ts-context-commentstring
---   context_commentstring = {
---     enable = true,
---   },
---   -- p00f/nvim-ts-rainbow
---   rainbow = {
---     enable = true,
---     exteded_mode = true,
---     max_file_lines = 10000,
---   },
--- })
---
+++ nvim/lua/keymap/neorg.lua
@@ -0,0 +1,16 @@
+local nmap = function(lhs, rhs, desc)
+  vim.keymap.set('n', lhs, rhs, { buffer = true, desc = desc })
+end
+
+nmap('<Leader>td', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_done<CR>', 'task done')
+nmap('<Leader>tu', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_undone<CR>', 'task undone')
+nmap('<Leader>th', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_onhold<CR>', 'task onhold')
+nmap('<Leader>tc', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_cancelled<CR>', 'task cancel')
+nmap('<Leader>tp', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_pending<CR>', 'task pending')
+nmap('<Leader>tr', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_recurring<CR>', 'task recurring')
+nmap('<Leader>ti', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_important<CR>', 'task important')
+nmap('<Leader><Leader>', '<Cmd>Neorg keybind norg core.norg.qol.todo_items.todo.task_cycle<CR>', 'task cycle')
+nmap('<C-S-l>', '<Cmd>Neorg keybind norg core.promo.promote<CR>', 'header promote')
+nmap('<C-S-h>', '<Cmd>Neorg keybind norg core.promo.demote<CR>', 'header demote')
+nmap('<C-S-j>', '<Cmd>Neorg keybind norg core.integrations.treesitter.next.heading<CR>', 'next heading')
+nmap('<C-S-k>', '<Cmd>Neorg keybind norg core.integrations.treesitter.previous.heading<CR>', 'previous heading')
