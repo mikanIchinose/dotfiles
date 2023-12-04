d84321318d80125df619f77ece00d8b8f828c936
Ichinose Yoshihiro <maruisansmai@gmail.com>
2023-12-04 12:20:10 +0900

    update

--- home/config/wezterm/wezterm.lua
+++ home/config/wezterm/wezterm.lua
@@ -1,20 +1,19 @@
 local wezterm = require('wezterm')
 
-local bg_image_path = ''
-local weekdayNum = tonumber(wezterm.strftime("%u"))
-if weekdayNum > 5 then
-  -- weekend
-  bg_image_path = wezterm.home_dir .. '/Pictures/wallpaper/overload_holy_kingdom.jpg'
-else
-  -- weekday
-  bg_image_path = wezterm.home_dir .. '/Pictures/wallpaper/vim.jpeg'
-end
+-- local bg_image_path = ''
+-- local weekdayNum = tonumber(wezterm.strftime("%u"))
+-- if weekdayNum > 5 then
+--   -- weekend
+--   bg_image_path = wezterm.home_dir .. '/Pictures/wallpaper/overload_holy_kingdom.jpg'
+-- else
+--   -- weekday
+--   bg_image_path = wezterm.home_dir .. '/Pictures/wallpaper/vim.jpeg'
+-- end
 
 return {
   audible_bell = "Disabled",
   color_scheme = 'iceberg-dark',
   font = wezterm.font('PleckJP', { weight = 'Regular' }),
-  --font = wezterm.font('JetBrains Mono', { weight = 'Regular' }),
   font_size = 25.0,
   enable_tab_bar = false,
   window_padding = {
@@ -23,8 +22,13 @@ return {
     top = 0,
     bottom = 0,
   },
-  window_background_image = bg_image_path,
-  window_background_image_hsb = {
-    brightness = 0.03,
-  },
+  macos_window_background_blur = 100,
+  window_background_opacity = 0.85,
+  --window_background_image = wezterm.home_dir .. '/Pictures/wallpaper/vim.jpeg',
+  --window_background_image_hsb = {
+  --  brightness = 0.01,
+  --},
+  -- background = {
+  --   horizontal_align = "Center",
+  -- },
 }
