d6e5560f6833181800ddcf06a3d35e4a8f9bf0d6
Ichinose Yoshihiro <maruisansmai@gmail.com>
2024-03-03 12:02:40 +0900

    update(nvim)

--- home/config/nvim_next/rc/plugins/ddu.ts
+++ home/config/nvim_next/rc/plugins/ddu.ts
@@ -3,8 +3,8 @@ import {
   ActionFlags,
   BaseActionParams,
   BaseConfig,
-} from "https://deno.land/x/ddu_vim@v3.7.0/types.ts";
-import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.7.0/base/config.ts";
+} from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
+import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.10.2/base/config.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 import { Params as FfParams } from "https://deno.land/x/ddu_ui_ff@v1.1.0/ff.ts";
 import { Params as FilerParams } from "https://deno.land/x/ddu_ui_filer@v1.1.0/filer.ts";
@@ -80,6 +80,7 @@ export class Config extends BaseConfig {
             ".next",
             ".idea",
             "node_modules",
+            "target",
           ],
         },
       },
