f78596438dbc4e27855bcba6af3db24083be7242
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-01-07 00:28:16 +0900

    delete unmaintained script

--- __script/installer/flutter.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\n--------- flutter ---------\n"
-sudo snap install flutter --classic
-echo -e "\n--------- Finished!! ---------\n"
--- __script/installer/obs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install ffmpeg &&
-sudo add-apt-repository ppa:obsproject/obs-studio &&
-sudo apt update &&
-sudo apt install obs-studio
