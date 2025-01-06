c8e40393a1a9e1c643000c82135e77df7c183400
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-01-06 14:38:09 +0900

    update

--- install.sh
+++ install.sh
@@ -4,14 +4,24 @@ set -e
 set -u
 
 # install xcode command line tools
-xcode-select --install
+pathXCodeCommandLineTools=$(xcode-select -p 2>&1)
+if ["$pathXCodeCommandLineTools" != "/Library/Developer/CommandLineTools"]; then
+  echo "xcode-select --install"
+  xcode-select --install
+  read -p "Installing XCode Command Line Tools..."
+fi
 # install rosetta
+echo "softwareupdate --install-rosetta"
 softwareupdate --install-rosetta
 # install nix
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+if ! command -v nix 2>&1 >/dev/null
+then
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+fi
 # clone dotfiles
 nix-shell -p git --run "git clone https://github.com/mikanIchinose/dotfiles.git ~/dotfiles"
 nix run nix-darwin -- switch --flake ~/dotfiles#mikan
 # install dotfiles
-go install github.com/rhysd/dotfiles
+/run/current-system/sw/bin/go install github.com/rhysd/dotfiles
 ~/go/bin/dotfiles link ~/dotfiles
