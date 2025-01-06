a2e88cf5c78cb2d9c52aa393a16a3decdacdf1cc
mikan <maruisansmai@gmail.com>
2025-01-06 20:17:42 +0900

    Create uninstall.sh
    add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+# uninstall homebrew
+brew uninstall --force $(brew list)
+
+# uninstall nix-darwin
+nix --extra-experimental-features "nix-command flakes" run nix-darwin#darwin-uninstaller --no-confirm
+
+# uninstall nix-store
+nix-store --gc
+
+# uninstall nix
+/nix/nix-installer uninstall --no-confirm
