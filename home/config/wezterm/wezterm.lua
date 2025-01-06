eb0d11b54cfa36002c7e124bad636d57ddcc21e9
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-01-07 00:32:39 +0900

    update

--- home/config/wezterm/wezterm.lua
+++ home/config/wezterm/wezterm.lua
@@ -1,34 +1,14 @@
-local wezterm = require('wezterm')
-
--- local bg_image_path = ''
--- local weekdayNum = tonumber(wezterm.strftime("%u"))
--- if weekdayNum > 5 then
---   -- weekend
---   bg_image_path = wezterm.home_dir .. '/Pictures/wallpaper/overload_holy_kingdom.jpg'
--- else
---   -- weekday
---   bg_image_path = wezterm.home_dir .. '/Pictures/wallpaper/vim.jpeg'
--- end
-
 return {
   audible_bell = "Disabled",
   color_scheme = 'iceberg-dark',
   --font = wezterm.font('PleckJP', { weight = 'Regular' }),
-  font_size = 25.0,
-  enable_tab_bar = true,
+  font_size = 15.0,
+  enable_tab_bar = false,
   window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
   },
-  --macos_window_background_blur = 100,
-  --window_background_opacity = 0.85,
-  --window_background_image = wezterm.home_dir .. '/Pictures/wallpaper/vim.jpeg',
-  --window_background_image_hsb = {
-  --  brightness = 0.01,
-  --},
-  -- background = {
-  --   horizontal_align = "Center",
-  -- },
+  window_background_opacity = 0.9,
 }
