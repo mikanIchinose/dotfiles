935356e93a8de5cb4e1df70c05b8c60d581f5876
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-04-22 00:10:16 +0900

    update(wezterm)

--- home/config/wezterm/wezterm.lua
+++ home/config/wezterm/wezterm.lua
@@ -1,16 +1,25 @@
 local wezterm = require 'wezterm'
+
 return {
+  -- macskkと干渉しないようにする
+  -- https://zenn.dev/vim_jp/articles/wezterm-karabiner
+  use_ime = false,
   audible_bell = "Disabled",
   color_scheme = 'iceberg-dark',
   font = wezterm.font('PlemolJP Console NF', { weight = 'Regular' }),
   -- font = wezterm.font('UDEV Gothic 35NFLG', { weight = 'Regular' }),
   font_size = 15.0,
   enable_tab_bar = false,
-  window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
-  },
-  window_background_opacity = 0.9,
-}
+  window_background_opacity = 0.8,
+  macos_window_background_blur = 30,
+  -- window_background_image = "/Users/mikan/.config/wezterm/background.jpg",
+  -- background = {
+  --   {
+  --     source = {
+  --       File = "/Users/mikan/.config/wezterm/background.jpg",
+  --     },
+  --   },
+  -- },
+  macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
+ }
+
