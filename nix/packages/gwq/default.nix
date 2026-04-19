636234deceb18b77698c4b5ceea45535802c3996
GitHub Action <actions@github.com>
2026-04-19 01:37:41 +0000

    nix: update gwq 0.0.18 → 0.0.19

--- nix/packages/gwq/default.nix
+++ nix/packages/gwq/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "gwq";
-  version = "0.0.18";
+  version = "0.0.19";
 
   src = fetchFromGitHub {
     owner = "d-kuro";
     repo = "gwq";
     rev = "v${version}";
-    hash = "sha256-ZuzDKR3GYbnfhQHkQ7bfiSkSArIRXywjvSWza6OK8Vg=";
+    hash = "sha256-2uE04frxfvQBlrOg5d0hPzGE9sbpzxHEiCeJX1ilG2M=";
   };
 
   vendorHash = "sha256-4K01Xf1EXl/NVX1loQ76l1bW8QglBAQdvlZSo7J4NPI=";
