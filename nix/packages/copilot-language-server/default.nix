4e98aeaeadba7842b33737e26ef141d97a535eda
GitHub Action <actions@github.com>
2026-07-18 01:35:54 +0000

    nix: update copilot-language-server 1.522.0 → 1.523.0

--- nix/packages/copilot-language-server/default.nix
+++ nix/packages/copilot-language-server/default.nix
@@ -6,11 +6,11 @@
 
 buildNpmPackage {
   pname = "copilot-language-server";
-  version = "1.522.0";
+  version = "1.523.0";
 
   src = ./.;
 
-  npmDepsHash = "sha256-ivE4KPeBy8ezAFCjrmDAT5+Y8Yx4QtqX1NTOcb9fAgU=";
+  npmDepsHash = "sha256-itgQZ1093bZVK9CR8Ely5OMHyjKfMZFkx0aU2dgjbug=";
 
   nodejs = nodejs_24;
 
